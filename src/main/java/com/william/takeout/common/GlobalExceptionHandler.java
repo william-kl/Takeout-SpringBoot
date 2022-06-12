60b3c3f7aab3c9f1b70ed5a6db0cdd182b623f0c
william <williamcai2029@gmail.com>
2022-06-12 14:05:20 -0400

    comment for add new employee

--- src/main/java/com/william/takeout/common/GlobalExceptionHandler.java
+++ src/main/java/com/william/takeout/common/GlobalExceptionHandler.java
@@ -19,13 +19,19 @@ public class GlobalExceptionHandler {
     // 解决 字段username被唯一索引约束的情况下，添加相同的username，抛出SQLIntegrityConstraintViolationException 的全局异常
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException e){
-        log.info(e.getMessage());
+        //在写具体类的时候，可以先在log.error加一个断点；先保证这个类能起作用（debug模式，输入duplicate
+        // username,提交后直接就跳过来了），然后再具体写处理逻辑
+        //这里要写log.error
+        log.error(e.getMessage());
 
+        //如果异常信息里含有这个关键字
         if (e.getMessage().contains("Duplicate entry")){
+            //异常信息用空格分隔，返回string数组
             String[] split = e.getMessage().split(" ");
             //zhangsan已存在
             String msg = split[2] + "已存在";//Duplicate entry 'zhangsan' for key 'idx_username'
-            return Result.error(msg);       //split[2] = zhangsan就拿到了
+            //返回给前端‘zhangsan’ 已存在        split[2] = zhangsan就拿到了
+            return Result.error(msg);
         }
         return Result.error("未知错误！");
     }
